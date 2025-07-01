545a5226cbc95da5a936c84d40cb12ae75944510
TehBotolxESSE <creamynald@gmail.com>
2025-07-01 16:17:04 +0700

    (add) maintenace for /

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #006400; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>MAINTENANCE</h1>
+    <div>
+        <p>Maaf atas ketidaknyamanannya, tetapi kami sedang melakukan pemeliharaan saat ini. Jika Anda memerlukan bantuan, Anda dapat menghubungi kami melalui <a href="mailto:creamynald@gmail.com">email</a>, jika tidak, kami akan segera kembali online!</p>
+        <p>&mdash; Tim IT Pengadilan Agama Natuna</p>
+    </div>
+</article>
