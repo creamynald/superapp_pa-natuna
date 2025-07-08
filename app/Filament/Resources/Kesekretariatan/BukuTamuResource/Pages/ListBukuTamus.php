9195dc54829354fb33d3f42385cc8e794ed6b430
TehBotolxESSE <creamynald@gmail.com>
2025-07-08 10:53:02 +0700

    (chg) style button buku tamu

--- app/Filament/Resources/Kesekretariatan/BukuTamuResource/Pages/ListBukuTamus.php
+++ app/Filament/Resources/Kesekretariatan/BukuTamuResource/Pages/ListBukuTamus.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Kesekretariatan\BukuTamuResource\Pages;
 use App\Filament\Resources\Kesekretariatan\BukuTamuResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\Kesekretariatan\BukuTamuResource\Actions\CreateAction;
 
 class ListBukuTamus extends ListRecords
 {
@@ -13,7 +14,14 @@ class ListBukuTamus extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make(
+            )->label('Tambah Tamu Baru') // Ganti label
+                ->icon('heroicon-o-plus-circle') // Ganti ikon
+                ->color('success') // Warna tombol (primary, success, danger, warning)
+                ->url(fn (): string => $this->getResource()::getUrl('create')) // opsional
+                ->visible(fn (): bool => auth()->user()->can('create_buku_tamu')
+            ),    
+
         ];
     }
 }
