67312b797a6566db3e1de03e56c7caa0914d0d80
TehBotolxESSE <creamynald@gmail.com>
2025-08-11 11:25:23 +0700

    (chg) path admin to mimin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('mimin')
             ->login()
             ->colors([
                 'primary' => Color::Green,
