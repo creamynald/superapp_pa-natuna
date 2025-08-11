35eee6cfc43bac6ba785c5218d734304e1bd3129
TehBotolxESSE <creamynald@gmail.com>
2025-08-11 11:20:05 +0700

    (example) fonnte

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
     return view('maintenance');
+});
+
+Route::get('/test-fonnte', function () {
+    $response = Http::withHeaders([
+            'Authorization' => env('FONNTE_TOKEN'),
+        ])
+        ->asMultipart()
+        ->post('https://api.fonnte.com/send', [
+            'target'      => '085263216699', // nomor tujuan
+            'message'     => 'test message dari Laravel 12',
+            'countryCode' => '62',
+        ]);
+
+    return [
+        'ok'     => $response->successful(),
+        'status' => $response->status(),
+        'body'   => $response->body(),
+        'json'   => $response->json(),
+    ];
 });
